7aa23ac0d49f60fdff2796de3a3b0234d3ca12f3
Deepesh Hada <deepesh.hada@gmail.com>
2020-06-19 17:58:35 +0530

    Added function that detects objects through the trained model given an input NumPy image

--- utils.py
+++ utils.py
@@ -6,95 +6,66 @@ import matplotlib.patches as patches
 
 
 def boxes_iou(box1, box2):
-  
-    # Get the Width and Height of each bounding box
     width_box1 = box1[2]
     height_box1 = box1[3]
     width_box2 = box2[2]
     height_box2 = box2[3]
-    
-    # Calculate the area of the each bounding box
+
     area_box1 = width_box1 * height_box1
     area_box2 = width_box2 * height_box2
-    
-    # Find the vertical edges of the union of the two bounding boxes
-    mx = min(box1[0] - width_box1/2.0, box2[0] - width_box2/2.0)
-    Mx = max(box1[0] + width_box1/2.0, box2[0] + width_box2/2.0)
-    
-    # Calculate the width of the union of the two bounding boxes
+
+    mx = min(box1[0] - width_box1 / 2.0, box2[0] - width_box2 / 2.0)
+    Mx = max(box1[0] + width_box1 / 2.0, box2[0] + width_box2 / 2.0)
+    my = min(box1[1] - height_box1 / 2.0, box2[1] - height_box2 / 2.0)
+    My = max(box1[1] + height_box1 / 2.0, box2[1] + height_box2 / 2.0)
     union_width = Mx - mx
-    
-    # Find the horizontal edges of the union of the two bounding boxes
-    my = min(box1[1] - height_box1/2.0, box2[1] - height_box2/2.0)
-    My = max(box1[1] + height_box1/2.0, box2[1] + height_box2/2.0)    
-    
-    # Calculate the height of the union of the two bounding boxes
     union_height = My - my
-    
-    # Calculate the width and height of the area of intersection of the two bounding boxes
+
     intersection_width = width_box1 + width_box2 - union_width
     intersection_height = height_box1 + height_box2 - union_height
-   
-    # If the the boxes don't overlap then their IOU is zero
     if intersection_width <= 0 or intersection_height <= 0:
         return 0.0
 
-    # Calculate the area of intersection of the two bounding boxes
     intersection_area = intersection_width * intersection_height
-    
-    # Calculate the area of the union of the two bounding boxes
     union_area = area_box1 + area_box2 - intersection_area
-    
-    # Calculate the IOU
-    iou = intersection_area/union_area
-    
+    iou = intersection_area / union_area
+
     return iou
 
 
 def nms(boxes, iou_thresh):
-    
-    # If there are no bounding boxes do nothing
     if len(boxes) == 0:
         return boxes
-    
-    # Create a PyTorch Tensor to keep track of the detection confidence
-    # of each predicted bounding box
+
     det_confs = torch.zeros(len(boxes))
-    
-    # Get the detection confidence of each predicted bounding box
     for i in range(len(boxes)):
         det_confs[i] = boxes[i][4]
+    _, sortIds = torch.sort(det_confs, descending=True)
 
-    # Sort the indices of the bounding boxes by detection confidence value in descending order.
-    # We ignore the first returned element since we are only interested in the sorted indices
-    _,sortIds = torch.sort(det_confs, descending = True)
-    
-    # Create an empty list to hold the best bounding boxes after
-    # Non-Maximal Suppression (NMS) is performed
     best_boxes = []
-    
-    # Perform Non-Maximal Suppression 
     for i in range(len(boxes)):
-        
-        # Get the bounding box with the highest detection confidence first
         box_i = boxes[sortIds[i]]
-        
-        # Check that the detection confidence is not zero
         if box_i[4] > 0:
-            
-            # Save the bounding box 
             best_boxes.append(box_i)
-            
-            # Go through the rest of the bounding boxes in the list and calculate their IOU with
-            # respect to the previous selected box_i. 
             for j in range(i + 1, len(boxes)):
                 box_j = boxes[sortIds[j]]
-                
-                # If the IOU of box_i and box_j is higher than the given IOU threshold set
-                # box_j's detection confidence to zero. 
                 if boxes_iou(box_i, box_j) > iou_thresh:
                     box_j[4] = 0
-                    
+
     return best_boxes
 
 
+def detect_objects(model, img, iou_thresh, nms_thresh):
+    start = time.time()
+    model.eval()
+    img = torch.from_numpy(img.transpose(2, 0, 1)).float().div(255.0).unsqueeze(0)
+    list_boxes = model(img, nms_thresh)
+    boxes = list_boxes[0][0] + list_boxes[1][0] + list_boxes[2][0]
+    boxes = nms(boxes, iou_thresh)
+    finish = time.time()
+
+    print('\n\nIt took {:.3f}'.format(finish - start),
+          'seconds to detect the objects in the image.\n')
+
+    print('Number of Objects Detected:', len(boxes), '\n')
+    return boxes
