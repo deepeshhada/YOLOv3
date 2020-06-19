529817fe97b82d27455e6f9a7761f7d470962adf
Deepesh Hada <deepesh.hada@gmail.com>
2020-06-19 18:03:22 +0530

    Added additional utils to load class names from file, print objects and plot boxes in an input image after object detection

--- utils.py
+++ utils.py
@@ -69,3 +69,84 @@ def detect_objects(model, img, iou_thresh, nms_thresh):
 
     print('Number of Objects Detected:', len(boxes), '\n')
     return boxes
+
+
+def load_class_names(namesfile):
+    class_names = []
+    with open(namesfile, 'r') as fp:
+        lines = fp.readlines()
+    for line in lines:
+        line = line.rstrip()
+        class_names.append(line)
+    return class_names
+
+
+def print_objects(boxes, class_names):    
+    print('Objects Found and Confidence Level:\n')
+    for i in range(len(boxes)):
+        box = boxes[i]
+        if len(box) >= 7 and class_names:
+            cls_conf = box[5]
+            cls_id = box[6]
+            print('%i. %s: %f' % (i + 1, class_names[cls_id], cls_conf))
+
+            
+def plot_boxes(img, boxes, class_names, plot_labels, color = None):
+    colors = torch.FloatTensor([[1,0,1],[0,0,1],[0,1,1],[0,1,0],[1,1,0],[1,0,0]])
+    def get_color(c, x, max_val):
+        ratio = float(x) / max_val * 5
+        i = int(np.floor(ratio))
+        j = int(np.ceil(ratio))
+        ratio = ratio - i
+        r = (1 - ratio) * colors[i][c] + ratio * colors[j][c]
+        return int(r * 255)
+    
+    width = img.shape[1]
+    height = img.shape[0]
+    fig, a = plt.subplots(1,1)
+    a.imshow(img)
+
+    for i in range(len(boxes)):
+        box = boxes[i]
+        x1 = int(np.around((box[0] - box[2]/2.0) * width))
+        y1 = int(np.around((box[1] - box[3]/2.0) * height))
+        x2 = int(np.around((box[0] + box[2]/2.0) * width))
+        y2 = int(np.around((box[1] + box[3]/2.0) * height))
+        rgb = (1, 0, 0)
+        if len(box) >= 7 and class_names:
+            cls_conf = box[5]
+            cls_id = box[6]
+            classes = len(class_names)
+            offset = cls_id * 123457 % classes
+            red   = get_color(2, offset, classes) / 255
+            green = get_color(1, offset, classes) / 255
+            blue  = get_color(0, offset, classes) / 255
+            
+            if color is None:
+                rgb = (red, green, blue)
+            else:
+                rgb = color
+        width_x = x2 - x1
+        width_y = y1 - y2
+        rect = patches.Rectangle(
+			(x1, y2),
+            width_x, width_y,
+            linewidth = 2,
+            edgecolor = rgb,
+            facecolor = 'none'
+		)
+
+        a.add_patch(rect)
+        if plot_labels:
+            conf_tx = class_names[cls_id] + ': {:.1f}'.format(cls_conf)
+            lxc = (img.shape[1] * 0.266) / 100
+            lyc = (img.shape[0] * 1.180) / 100
+            a.text(
+                x1 + lxc, 
+                y1 - lyc, 
+                conf_tx, 
+                fontsize = 24, 
+                color = 'k',
+                bbox = dict(facecolor = rgb, edgecolor = rgb, alpha = 0.8)
+            )        
+    plt.show()
